758cdc972f456d340b8d06b7b074ee4c8445d18a
ninellekam <nina.kamkia@mail.ru>
2021-06-21 13:42:50 +0300

    contest

--- sums.cpp
+++ sums.cpp
@@ -2,49 +2,56 @@
 #include <vector>
 #include <map>
 #include <set>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 int	FindConvMinSum(std::vector <int> & vec, int k) {
-	std::unordered_set <int> Set;
+	std::unordered_map <int, int> Set;
 	size_t i = 0, j = 0, len = vec.size();
 	int min_sum = INT_MAX, sum = 0;
 
 	while (i < len) {
 		if (vec[i] <= k) {
-			if (Set.find(vec[i]) == Set.end()) {
-				Set.insert(vec[i]);
-			}
+				++Set[vec[i]];
+				std::cout << " IN " << vec[i] << "\n";
 		}
 		sum += vec[i];
 		if (Set.size() == k) {
-			if (sum < min_sum)
+			if (sum < min_sum) {
 				min_sum = sum;
+				std::cout << "\n!!!!!!!!!!!!!!!!!!MIN SUM " << min_sum << "\n";
+			}
 
-			while (j < i) {
-				std::cout << sum << " ";
+			while (Set.size() == k) {
+				std::cout << sum << "";
 				sum -= vec[j];
 
-				if (vec[j] <= k)
-					Set.erase(vec[j]);
+				if (vec[j] <= k) {
+					--Set[vec[j]];
+					if (Set[vec[j]] == 0) {
+						Set.erase(vec[j]);
+						for (auto it : Set)
+							std::cout << " ELEMENT " << it.first << " ";
+					}
+				}
 
 				std::cout << " - " << vec[j] << " = " << sum << " \n";
 				if (Set.size() == k)
 					if (sum < min_sum) {
 						min_sum = sum;
-						std::cout << "MIN SUM " << min_sum << "\n";
+						std::cout << "\nMIN SUM " << min_sum << "\n";
 					}
 				++j;
 			}
 		}
 		++i;
 	}
-	std::cout << "\nanswer is a " << min_sum << "\n";
+	std::cout << "\nanswer is a " << min_sum << "!!!!\n";
 	return min_sum;
 }
 
 int main() {
-	std::vector <int> vec = {1, 2, 5, 6, 4, 3, 2, 1};
+	std::vector <int> vec = {1, 2,2, 3, 5, 0, 0};
 	FindConvMinSum(vec, 3);
 }
