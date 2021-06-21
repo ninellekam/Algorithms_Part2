d09ea6b13215734248f2d98cee17919f5096236c
ninellekam <nina.kamkia@mail.ru>
2021-06-21 14:06:41 +0300

    hard cpp

--- Hard.cpp
+++ Hard.cpp
@@ -12,46 +12,52 @@ int	FindConvMinSum(std::vector <int> & vec, int k) {
 	int min_sum = INT_MAX, sum = 0;
 
 	while (i < len) {
-		if (vec[i] <= k) {
+		if (vec[i] <= k)
 				++Set[vec[i]];
-				std::cout << " IN " << vec[i] << "\n";
-		}
+
 		sum += vec[i];
+
 		if (Set.size() == k) {
-			if (sum < min_sum) {
+			if (sum < min_sum)
 				min_sum = sum;
-				std::cout << "\n!!!!!!!!!!!!!!!!!!MIN SUM " << min_sum << "\n";
-			}
 
 			while (Set.size() == k) {
-				std::cout << sum << "";
 				sum -= vec[j];
 
 				if (vec[j] <= k) {
 					--Set[vec[j]];
-					if (Set[vec[j]] == 0) {
+					if (Set[vec[j]] == 0)
 						Set.erase(vec[j]);
-						for (auto it : Set)
-							std::cout << " ELEMENT " << it.first << " ";
-					}
 				}
 
-				std::cout << " - " << vec[j] << " = " << sum << " \n";
 				if (Set.size() == k)
-					if (sum < min_sum) {
+					if (sum < min_sum)
 						min_sum = sum;
-						std::cout << "\nMIN SUM " << min_sum << "\n";
-					}
+
 				++j;
 			}
 		}
 		++i;
 	}
-	std::cout << "\nanswer is a " << min_sum << "!!!!\n";
+	std::cout << "\nAnswer is a " << min_sum << "!!!!\n";
 	return min_sum;
 }
 
 int main() {
-	std::vector <int> vec = {1, 2,2, 3, 5, 0, 0};
-	FindConvMinSum(vec, 3);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int cnt_elems, vec_elems, k = 3;
+	// std::vector <int> vec;
+	// std::cin >> cnt_elems;
+
+	// for(size_t i = 0; i < cnt_elems; ++i) {
+	// 	std::cin >> vec_elems;
+	// 	vec.push_back(vec_elems);
+	// }
+	// std::cin >> cnt_elems;
+
+// for simple:
+	std::vector <int> vec = {2, 2, 2, 1, 3, 1, 1, 1, 1, 1};
+	FindConvMinSum(vec, k);
 }
