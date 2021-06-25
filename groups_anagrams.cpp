987c0521dae41fad7ed1aff10f92ecadd179e7bb
ninellekam <nina.kamkia@mail.ru>
2021-06-25 21:30:35 +0300

    striiiings

---
+++ groups_anagrams.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+
+std::vector< std::vector<std::string> > groupAnagrams(std::vector<std::string>& strs) {
+	std::vector< std::vector<std::string> > v;
+	std::map <std::vector<int>, std::vector<std::string>> mp;
+
+	for(auto it : strs) {
+		std::vector<int> temp(26);
+
+		for(size_t i = 0; i < it.size(); ++i)
+			temp[it[i]-'a']++;
+
+		mp[temp].push_back(it);
+	}
+	
+	for (auto it : mp)
+		v.push_back(it.second);
+
+	return v;
+}
---
+++ subsets.cpp
@@ -0,0 +1,33 @@
+// Given an integer array nums of unique elements,
+// return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets.
+// Return the solution in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void generateSubsets(vector<int>& nums , int ind, vector<int> &st,  vector<vector <int> > &res){
+		res.push_back(st);
+
+		for (int i = ind; i < nums.size(); ++i) {
+			st.push_back(nums[i]);
+			generateSubsets(nums , i+1 , st, res);
+			st.erase(st.end() - 1);
+		}
+	}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> res;
+		vector<int> st;
+
+		generateSubsets(nums, 0, st, res);
+
+		return res;
+}
